39b03afb81bb052b89e83e1d2d11f5eead86c982
kenzo03 <kevinwijaya480@gmail.com>
2022-04-03 01:00:01 +0700

    call constant baseUrl

--- lib/data/api/api_service.dart
+++ lib/data/api/api_service.dart
@@ -1,12 +1,11 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../model/restaurant.dart';
+import '../../common/constant.dart';
 
 class ApiService {
-  static const String _baseUrl = 'https://restaurant-api.dicoding.dev/';
-
   Future<RestaurantList> getRestaurants() async {
-    final response = await http.get(Uri.parse(_baseUrl + "list"));
+    final response = await http.get(Uri.parse(Constant.baseUrl + "list"));
     if (response.statusCode == 200) {
       return RestaurantList.fromJson(json.decode(response.body));
     } else {
@@ -15,7 +14,8 @@ class ApiService {
   }
 
   Future<RestaurantDetail> getRestaurantDetail(String id) async {
-    final response = await http.get(Uri.parse(_baseUrl + "detail/" + id));
+    final response =
+        await http.get(Uri.parse(Constant.baseUrl + "detail/" + id));
     if (response.statusCode == 200) {
       return RestaurantDetail.fromJson(json.decode(response.body));
     } else {
@@ -26,7 +26,7 @@ class ApiService {
   Future<RestaurantSearch> searchRestaurant(String query) async {
     String tempQuery = query.replaceAll(' ', '%20');
     final response =
-        await http.get(Uri.parse(_baseUrl + "search?q=" + tempQuery));
+        await http.get(Uri.parse(Constant.baseUrl + "search?q=" + tempQuery));
     if (response.statusCode == 200) {
       return RestaurantSearch.fromJson(json.decode(response.body));
     } else {
@@ -37,7 +37,7 @@ class ApiService {
   Future<RestaurantList> addReview(
       String id, String name, String review) async {
     final response = await http.post(
-      Uri.parse(_baseUrl + "review"),
+      Uri.parse(Constant.baseUrl + "review"),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
